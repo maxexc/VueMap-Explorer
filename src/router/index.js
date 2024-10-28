ac86ab9b6c32be8a3cc48c6ad9cc34c91481d970
Maksym Solomko <104863585+maxexc@users.noreply.github.com>
2024-10-28 14:24:57 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 // ** Lazy Loading **
-const GeeringPage = () => import('../views/GreetingView.vue')
-const HomePage = () => import('../views/HomepageView.vue')
-const AuthPage = () => import('../views/AuthView.vue')
-const LoginPage = () => import('../views/LoginView.vue')
-const RegistrationPage = () => import('../views/Registration.vue')
+const GeeringPage = () => import('@/views/GreetingView.vue')
+const HomePage = () => import('@/views/HomepageView.vue')
+const AuthPage = () => import('@/views/AuthView.vue')
+const LoginPage = () => import('@/views/LoginView.vue')
+const RegistrationPage = () => import('@/views/Registration.vue')
 
 const BASE = import.meta.env.VITE_BASE_URL
 console.log('BASE: ', BASE);
