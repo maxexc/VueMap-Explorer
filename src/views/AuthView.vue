7fcab8b3d5435aa9990e0489aa39ed0bd965be9e
Maksym Solomko <max.exc7@gmail.com>
2024-10-29 11:56:20 +0100

    update

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -6,7 +6,6 @@ import BaseLayout from '@/layouts/BaseLayout.vue'
 <template>
   <BaseLayout>
     <section class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
-      <!-- <h2 class="text-center text-green-600">It`s AUTH PAGE</h2> -->
       <nav class="flex gap-11 justify-center mb-10 font-bold text-base">
         <RouterLink class="text-grey" exact-active-class="text-primary" to="registration">
           Create an account
--- vite.config.js
+++ vite.config.js
@@ -21,32 +21,3 @@ export default defineConfig({
   },
 });
 
-
-
-
-
-
-
-// import { fileURLToPath, URL } from 'node:url'
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: '/VueMap-Explorer/',
-//   plugins: [
-//     vue(),
-//     vueDevTools(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url)),
-//       src: fileURLToPath(new URL('./src', import.meta.url)),
-//       components: fileURLToPath(new URL('./src/components', import.meta.url)),
-//       views: fileURLToPath(new URL('./src/views', import.meta.url)),
-//       assets: fileURLToPath(new URL('./src/assets', import.meta.url))
-//     }
-//   },
-// })
-
