72584723d38c4b04eefe1685b53a8745eda932ec
Maksym Solomko <max.exc7@gmail.com>
2024-10-29 12:03:57 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import AuthView from '@/views/AuthView.vue';
-import LoginView from '@/views/LoginView.vue';
-import Registration from '@/views/Registration.vue';
+// import AuthView from '@/views/AuthView.vue';
+// import LoginView from '@/views/LoginView.vue';
+// import Registration from '@/views/Registration.vue';
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 // ** Lazy Loading **
 const GreetingPage = () => import('@/views/GreetingView.vue')
 const HomePage = () => import('@/views/HomepageView.vue')
-// const AuthPage = () => import('@/views/AuthView.vue')
-// const LoginPage = () => import('@/views/LoginView.vue')
-// const RegistrationPage = () => import('@/views/Registration.vue')
+const AuthPage = () => import('@/views/AuthView.vue')
+const LoginPage = () => import('@/views/LoginView.vue')
+const RegistrationPage = () => import('@/views/Registration.vue')
 
 const BASE = import.meta.env.VITE_BASE_URL
 console.log('BASE: ', BASE);
@@ -18,11 +18,11 @@ const routes = [
     { path: '/map', component: HomePage },
     {
         path: '/auth',
-        component: AuthView,
+        component: AuthPage,
         redirect: '/auth/login',
         children: [
-            { path: 'login', component: LoginView },
-            { path: 'registration', component: Registration }
+            { path: 'login', component: LoginPage },
+            { path: 'registration', component: RegistrationPage }
         ]
     },
 ]
@@ -31,4 +31,19 @@ export const router = createRouter({
     history: createWebHistory(import.meta.env.VITE_BASE_URL),
     routes,
 })
+
+
+// const routes = [
+//     { path: '/', component: GreetingPage },
+//     { path: '/map', component: HomePage },
+//     {
+//         path: '/auth',
+//         component: AuthView,
+//         redirect: '/auth/login',
+//         children: [
+//             { path: 'login', component: LoginView },
+//             { path: 'registration', component: Registration }
+//         ]
+//     },
+// ]
 
