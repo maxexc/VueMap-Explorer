e1911e7a273adb9c5b0391763651ac28b9449a01
Maksym Solomko <max.exc7@gmail.com>
2024-10-30 08:55:05 +0100

    patch

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,14 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { BASE } from '@/router/index.js'
+console.log('NotFoundView BASE', BASE)
+</script>
+
 <template>
   <div class="flex flex-col items-center justify-center text-center h-screen bg-white">
     <img
-      src="assets/img/404.jpg"
+      src="src/assets/img/404.jpg"
       alt="Page Not Found"
       class="mx-auto max-w-2xl rounded-3xl shadow-lg"
     />
     <h1 class="text-5xl font-bold mt-5 text-[#2C3E50]">404</h1>
     <p class="text-xl mt-2 text-[#333333]">Page Not Found</p>
     <router-link
-      to="/"
+      :to="`${BASE}/`"
       class="mt-5 bg-gradient-to-r from-secondary to-primary rounded-xl py-3 px-10 font-bold tracking-wider text-white overflow-hidden shadow-md transform transition duration-200 hover:shadow-lg active:scale-95"
     >
       Back to Home Page
