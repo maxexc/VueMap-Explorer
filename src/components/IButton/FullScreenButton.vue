8fd7eb29f939be1a9e7aac44481fea93815e3c83
Maksym Solomko <max.exc7@gmail.com>
2024-11-19 02:32:18 +0100

    Full Screen button
    new mode

--- src/components/IButton/FullScreenButton.vue
+++ src/components/IButton/FullScreenButton.vue
@@ -1,18 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 
-// Локальная переменная для отслеживания полноэкранного режима
 const isFullscreen = ref(false)
 
 const toggleFullscreen = () => {
-  const element = document.documentElement // Полноэкранный режим для документа
+  const element = document.documentElement
   if (!isFullscreen.value) {
     if (element.requestFullscreen) {
       element.requestFullscreen()
     } else if (element.webkitRequestFullscreen) {
-      element.webkitRequestFullscreen() // Для Safari
+      element.webkitRequestFullscreen()
     } else if (element.msRequestFullscreen) {
-      element.msRequestFullscreen() // Для старых версий IE/Edge
+      element.msRequestFullscreen()
     } else {
       alert('Fullscreen is not supported on your browser.')
     }
@@ -34,7 +33,7 @@ const toggleFullscreen = () => {
   <button
     v-button-animation
     @click="toggleFullscreen"
-    class="z-10 bg-white/80 shadow-lg hover:bg-gray-100 transition transform hover:scale-105"
+    class="z-10 bg-white/80 backdrop-blur-[2px] rounded shadow-lg hover:bg-gray-100 transition transform hover:scale-105"
     title="Toggle Fullscreen"
   >
     <svg
@@ -43,7 +42,7 @@ const toggleFullscreen = () => {
       width="32"
       height="32"
       fill="#5e5e5e"
-      viewBox="-1 -1 34 34"
+      viewBox="-2 -2 36 36"
       stroke="#fff"
       stroke-width="1"
     >
@@ -59,7 +58,7 @@ const toggleFullscreen = () => {
       fill="none"
       stroke="#5e5e5e"
       stroke-width="1"
-      viewBox="-1 -1 34 34"
+      viewBox="-2 -2 36 36"
     >
       <path
         d="M11.148 17.988l-6.58 6.58-2.568-2.568c-1.105 0-2 0.895-2 2v6c0 1.105 0.894 2 2 2h6c1.105 0 2-0.895 2-2l-2.568-2.566 6.58-6.582-2.863-2.863zM30 0h-6c-1.105 0-2 0.895-2 2l2.568 2.568-6.58 6.58 2.863 2.863 6.58-6.579 2.568 2.567c1.105 0 2-0.895 2-2v-6c0-1.105-0.895-2-2-2zM30 22l-2.568 2.568-6.58-6.58-2.863 2.863 6.58 6.582-2.568 2.567c0 1.105 0.895 2 2 2h6c1.105 0 2-0.895 2-2v-6c0-1.105-0.895-2-2-2zM10 2c0-1.105-0.895-2-2-2h-6c-1.105 0-2 0.895-2 2v6c0 1.105 0.894 2 2 2l2.568-2.567 6.58 6.579 2.863-2.863-6.58-6.58 2.568-2.568z"
