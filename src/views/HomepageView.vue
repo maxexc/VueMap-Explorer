f409ef125eb13dc546f175663be014bf0bd18b1a
Maksym Solomko <max.exc7@gmail.com>
2025-04-03 07:41:15 +0200

    update

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -524,7 +524,11 @@ async function saveGeojsonToFile(geojson) {
           </MapboxMarker>
           <MapboxNavigationControl position="bottom-right" :showZoom="false" :showCompass="true" />
           <ResetZoomButton class="absolute" :mapInstance="mapInstance" :defaultZoom="10" />
-          <Toggle3DButton class="absolute" :is3DEnabled="is3DEnabled" @toggle3D="toggle3D" />
+          <Toggle3DButton
+            :class="routeStore.isIOS ? 'fixed' : 'absolute'"
+            :is3DEnabled="is3DEnabled"
+            @toggle3D="toggle3D"
+          />
           <FullScreenButton class="absolute bottom-[151px] right-[13px]" />
           <MapboxGeolocateControl
             position="bottom-left"
